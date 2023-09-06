368a5b0cee83bb368e42b0f7233a9cc82907d253
Baronous <117913699+Baronous@users.noreply.github.com>
2023-09-06 19:32:53 +0700

    Add files via upload

---
+++ TEST/Bai1.java
@@ -0,0 +1,10 @@
+package TEST;
+import java.util.Scanner;
+public class Bai1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Nhap chuoi ky tu:");
+            String str = scanner.nextLine();
+
+        }
+}
